51404ce0291e82673873d2aebec04d86968e9f62
o328r0mhef <73085035+o328r0mhef@users.noreply.github.com>
2024-05-11 20:51:09 +0100

    Update adjust_labelling_to_new_resolution.py

--- Using_Coco_dataset/adjust_labelling_to_new_resolution.py
+++ Using_Coco_dataset/adjust_labelling_to_new_resolution.py
@@ -1,13 +1,38 @@
+"""
+File: adjust_labelling_to_new_resolution.py
+Author: Dylan Turland Cowell
+Date Created: 11-May-2024
+Description: This script is designed to be used with the 'resize_images.py' and 'custom_json_labelling.py' scripts.
+             After reducing Coco dataset image resolutions and generating a custom labelling file, running this script adjusts the bounding boxes 
+             to fit to the new resolution.
+"""
+
 import json
 
 def adjust_bbox(bbox, scale_factor):
-    # Adjust bounding box coordinates
+     """
+    Adjust bounding box coordinates by dividing each coordinate by the scale_factor.
+
+    Parameters:
+    - bbox (list): List containing [x_min, y_min, width, height] of the bounding box.
+    - scale_factor (float): Factor by which to scale down the bounding box coordinates.
+
+    Returns:
+    - adjusted_bbox (list): List containing adjusted [x_min, y_min, width, height].
+    """
     adjusted_bbox = [
         int(coord / scale_factor) for coord in bbox
     ]
     return adjusted_bbox
 
 def resize_annotations(json_file, scale_factor):
+    """
+    Resize annotations in a .json file by adjusting Coco bounding box coordinates.
+
+    Parameters:
+    - json_file (str): Path to the .json file containing annotations.
+    - scale_factor (float): Factor by which to scale down the bounding box coordinates.
+    """
     with open(json_file, 'r') as f:
         data = json.load(f)
 
@@ -19,14 +44,14 @@ def resize_annotations(json_file, scale_factor):
         annotation['bbox'] = adjusted_bbox
 
     # Save the modified JSON file
-    with open('C:/Users/Dylan/fiftyone/coco-2017_75000/train/data_240_max/bounding_boxes.json', 'w') as f:
+    with open('new_directory/bounding_boxes.json', 'w') as f:
         json.dump(data, f, indent=4)
 
-# Adjusted scale factor based on image resizing (original_resolution / new_resolution)
-scale_factor = 640 / 240  # Example: 640 pixels to 96 pixels
+# Adjusted scale factor from image resising (original_resolution / new_resolution)
+scale_factor = 640 / 240  # Example: 640 pixels to 240 pixels
 
-# Provide the path to your original JSON file containing bounding box annotations
-original_json_file = 'C:/Users/Dylan/fiftyone/coco-2017_75000/train/bounding_boxes_original.json'
+# Path to your original .json file containing Coco bounding box labelling
+original_json_file = 'directory_of_labelling_file_to_edit'
 
 # Resize the bounding box annotations and save the modified JSON file
 resize_annotations(original_json_file, scale_factor)
